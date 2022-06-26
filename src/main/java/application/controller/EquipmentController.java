788d5109b85bd94232567a0e9468d295c64e1a53
neido <neido@debian-netyai.netyai>
2022-06-26 17:22:35 -0300

    fix

--- src/main/java/application/controller/EquipmentController.java
+++ src/main/java/application/controller/EquipmentController.java
@@ -3,7 +3,7 @@ package application.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
-import application.serviice.EquipmentService;
+import application.service.EquipmentService;
 
 
 @RestController
